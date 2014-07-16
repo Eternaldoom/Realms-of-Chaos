ccceab34ffe58167454c8ec622bd747a4d288013
Eternaldoom <mezack@icloud.com>
2014-07-16 11:52:00 -1000

    Lol coding from iphone. I wonder how many errors I have created.

--- com/eternaldoom/realmsofchaos/overworld/blocks/ROCBlocks.java
+++ com/eternaldoom/realmsofchaos/overworld/blocks/ROCBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.block.material.Material;
 
 public class ROCBlocks {
 	public static OverworldBlock citronite_ore;
+       public static OverworldBlock citronite_block;
 	
 	public static OverworldBlock charwood_leaves;
 	public static OverworldBlock charwood_log;
@@ -17,12 +18,14 @@ public class ROCBlocks {
 		charwood_log = new BlockCharwoodLog();
 		charwood_sapling = new BlockCharwoodSapling();
 		netherrack_bricks = (OverworldBlock) new OverworldBlock(Material.rock, "realmsofchaos:netherrack_bricks", "bricksHellrock", 0.4f, 1.0f).setStepSound(Block.soundTypePiston);
+              citronite_block = new OverworldBlock(Material.glass, "realmsofchaos:citronite_block", "blockCitronite", 5.0f, 12.0f).setStepSound(OverworldBlock.soundTypeGlass);
 
 		citronite_ore.register("citronite_ore");
 		charwood_leaves.register("charwood_leaves");
 		charwood_log.register("charwood");
 		charwood_sapling.register("charwood_sapling");
 		netherrack_bricks.register("netherrack_bricks");
+              citronite_block.register("citronite_block");
 	}
 
 }
