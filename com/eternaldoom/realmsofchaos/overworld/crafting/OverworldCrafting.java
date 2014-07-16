4bb657e0ea6cc47f676f00b0a27cf3d37147fdcf
Eternaldoom <mezack@icloud.com>
2014-07-16 11:08:49 -1000

    Added recipe for netherrack bricks.

--- com/eternaldoom/realmsofchaos/overworld/crafting/OverworldCrafting.java
+++ com/eternaldoom/realmsofchaos/overworld/crafting/OverworldCrafting.java
@@ -1,12 +1,14 @@
 package com.eternaldoom.realmsofchaos.overworld.crafting;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import com.eternaldoom.realmsofchaos.overworld.blocks.ROCBlocks;
 
 public class OverworldCrafting {
 	
 	public static void initRecipes(){
 		//TODO: add recipes 
-		//GameRegistry.addRecipe();
+		GameRegistry.addRecipe(new ItemStack(ROCBlocks.netherrack_bricks, 4), new Object[]{"XX", "XX", 'X', Blocks.netherrack});
 	}
 
 }
