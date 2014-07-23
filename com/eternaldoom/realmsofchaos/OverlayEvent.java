668a24b1122879a6767d7a9945cdbb3047953677
Eternaldoom <mezack@icloud.com>
2014-07-23 12:49:02 -0700

    better visual effects for aquatic armor bonus

--- com/eternaldoom/realmsofchaos/OverlayEvent.java
+++ com/eternaldoom/realmsofchaos/OverlayEvent.java
@@ -1,6 +1,7 @@
 package com.eternaldoom.realmsofchaos;
 
 import net.minecraft.init.Blocks;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderBlockOverlayEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -14,4 +15,12 @@ public class OverlayEvent {
 		}
 	}
 
+	@SubscribeEvent
+	public void fogColors(EntityViewRenderEvent.FogColors evt){
+		if (evt.block == Blocks.water && ArmorBonusEvent.getAquatic()){
+			evt.red = 0.6f;
+			evt.green = 0.6f;
+			evt.blue = 0.7f;
+		}
+	}
 }
