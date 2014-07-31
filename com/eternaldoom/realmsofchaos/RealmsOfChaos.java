8fece5f05fa82afcd396e18d5d38e4ccd6858dde
Eternaldoom <mezack@icloud.com>
2014-07-30 17:28:06 -0700

    trying to fix

--- com/eternaldoom/realmsofchaos/RealmsOfChaos.java
+++ com/eternaldoom/realmsofchaos/RealmsOfChaos.java
@@ -1,5 +1,9 @@
 package com.eternaldoom.realmsofchaos;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.DimensionManager;
@@ -14,6 +18,9 @@ import com.eternaldoom.realmsofchaos.overworld.gen.OverworldGen;
 import com.eternaldoom.realmsofchaos.overworld.items.ROCItems;
 import com.eternaldoom.realmsofchaos.water.gen.BiomeGenWater;
 import com.eternaldoom.realmsofchaos.water.gen.WorldProviderWater;
+import com.jadarstudios.developercapes.DevCapes;
+import com.jadarstudios.developercapes.cape.CapeConfig;
+import com.jadarstudios.developercapes.cape.CapeConfigManager;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
@@ -64,6 +71,13 @@ public class RealmsOfChaos {
 	
 	@EventHandler
 	public void init(FMLInitializationEvent evt){
+		try{
+			InputStream is = DevCapes.getInstance().getStreamForURL(new URL("https://raw.githubusercontent.com/Eternaldoom/Realms-of-Chaos/master/capes.json"));
+			CapeConfig config = CapeConfigManager.INSTANCE.parseFromStream(is);
+			CapeConfigManager.INSTANCE.addConfig(CapeConfigManager.getUniqueId(), config);
+		}catch (MalformedURLException e){
+			e.printStackTrace();
+		}
 		
 	}
 	
--- com/eternaldoom/realmsofchaos/client/ClientProxy.java
+++ com/eternaldoom/realmsofchaos/client/ClientProxy.java
@@ -1,6 +1,13 @@
 package com.eternaldoom.realmsofchaos.client;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.eternaldoom.realmsofchaos.RealmsOfChaos;
 import com.jadarstudios.developercapes.DevCapes;
+import com.jadarstudios.developercapes.cape.CapeConfig;
+import com.jadarstudios.developercapes.cape.CapeConfigManager;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -12,7 +19,6 @@ public class ClientProxy {
 		System.out.println("[Realms of Chaos] Registering Renderers");
 		ROCKeyBindings.init();
 		Renderers.init();
-		DevCapes.getInstance().registerConfig("", identifier)
+		
 	}
-
 }
