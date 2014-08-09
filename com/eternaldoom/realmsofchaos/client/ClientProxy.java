ef1ae22f507cf1e6127ff6320260b5d2740ad7a5
Eternaldoom <mezack@icloud.com>
2014-08-09 09:44:59 -0700

    fixed server side crash

--- com/eternaldoom/realmsofchaos/client/ClientProxy.java
+++ com/eternaldoom/realmsofchaos/client/ClientProxy.java
@@ -20,5 +20,12 @@ public class ClientProxy {
 		ROCKeyBindings.init();
 		Renderers.init();
 		
+		try{
+			InputStream is = DevCapes.getInstance().getStreamForURL(new URL("https://dl.dropboxusercontent.com/s/b1dc1vykv0hfseg/capes.json"));
+			CapeConfig config = CapeConfigManager.INSTANCE.parseFromStream(is);
+			CapeConfigManager.INSTANCE.addConfig(CapeConfigManager.getUniqueId(), config);
+		}catch (MalformedURLException e){
+			e.printStackTrace();
+		}
 	}
 }
