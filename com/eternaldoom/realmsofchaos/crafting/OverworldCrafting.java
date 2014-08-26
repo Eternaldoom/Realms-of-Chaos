43228b78be0e02acc71437dca2455c2bc8383bd2
Eternaldoom <mezack@icloud.com>
2014-08-25 21:07:25 -0700

    a couple of recipes

--- com/eternaldoom/realmsofchaos/crafting/OverworldCrafting.java
+++ com/eternaldoom/realmsofchaos/crafting/OverworldCrafting.java
@@ -33,13 +33,16 @@ public class OverworldCrafting {
 		GameRegistry.addRecipe(new ItemStack(ROCItems.aquatic_orb, 9), new Object[]{"X", 'X', ROCBlocks.aquatic_block});
 		GameRegistry.addRecipe(new ItemStack(ROCBlocks.fractonite_block), new Object[]{"XXX", "XXX", "XXX", 'X', ROCItems.fractonite_stone});
 		GameRegistry.addRecipe(new ItemStack(ROCItems.fractonite_stone, 9), new Object[]{"X", 'X', ROCBlocks.fractonite_block});
-
+		GameRegistry.addRecipe(new ItemStack(ROCBlocks.osmaralt_block), new Object[]{"XXX", "XXX", "XXX", 'X', ROCItems.osmaralt_ingot});
+		GameRegistry.addRecipe(new ItemStack(ROCItems.osmaralt_ingot, 9), new Object[]{"X", 'X', ROCBlocks.osmaralt_block});
+		
 		RecipeHelper.addArmorRecipe(ROCItems.heliotrope_helmet, ROCItems.heliotrope_chestplate, ROCItems.heliotrope_leggings, ROCItems.heliotrope_boots, ROCItems.heliotrope);
 		RecipeHelper.addArmorRecipe(ROCItems.citronite_helmet, ROCItems.citronite_chestplate, ROCItems.citronite_leggings, ROCItems.citronite_boots, ROCItems.citronite_gem);
 		RecipeHelper.addArmorRecipe(ROCItems.xylite_helmet, ROCItems.xylite_chestplate, ROCItems.xylite_leggings, ROCItems.xylite_boots, ROCItems.xylite_ingot);
 		RecipeHelper.addArmorRecipe(ROCItems.flame_helmet, ROCItems.flame_chestplate, ROCItems.flame_leggings, ROCItems.heliotrope_boots, ROCItems.flame_stone);
 		RecipeHelper.addArmorRecipe(ROCItems.neptunite_helmet, ROCItems.neptunite_chestplate, ROCItems.neptunite_leggings, ROCItems.neptunite_boots, ROCItems.neptunite_ingot);
 		RecipeHelper.addArmorRecipe(ROCItems.aquatic_helmet, ROCItems.aquatic_chestplate, ROCItems.aquatic_leggings, ROCItems.aquatic_boots, ROCItems.aquatic_orb);
+		RecipeHelper.addArmorRecipe(ROCItems.osmaralt_helmet, ROCItems.osmaralt_chestplate, ROCItems.osmaralt_leggings, ROCItems.osmaralt_boots, ROCItems.osmaralt_ingot);
 		RecipeHelper.addArmorRecipe(ROCItems.fractonite_helmet, ROCItems.fractonite_chestplate, ROCItems.fractonite_leggings, ROCItems.fractonite_boots, ROCItems.fractonite_stone);
 		
 		RecipeHelper.addToolRecipe(ROCItems.heliotrope_sword, ROCItems.heliotrope_pickaxe, ROCItems.heliotrope_axe, ROCItems.heliotrope_shovel, ROCItems.heliotrope_bow, ROCItems.heliotrope_arrow, ROCItems.heliotrope);
@@ -48,6 +51,7 @@ public class OverworldCrafting {
 		RecipeHelper.addToolRecipe(ROCItems.flame_sword, ROCItems.flame_pickaxe, ROCItems.flame_axe, ROCItems.flame_shovel, ROCItems.flamestone_bow, ROCItems.flamestone_arrow, ROCItems.flame_stone, Items.lava_bucket);
 		RecipeHelper.addToolRecipe(ROCItems.neptunite_sword, ROCItems.neptunite_pickaxe, ROCItems.neptunite_axe, ROCItems.neptunite_shovel, ROCItems.neptunite_bow, ROCItems.neptunite_arrow, ROCItems.neptunite_ingot, ROCItems.fish_scale);
 		RecipeHelper.addToolRecipe(ROCItems.aquatic_sword, ROCItems.aquatic_pickaxe, ROCItems.aquatic_axe, ROCItems.aquatic_shovel, ROCItems.aquatic_bow, ROCItems.aquatic_arrow, ROCItems.aquatic_orb, ROCItems.fish_scale);
+		RecipeHelper.addToolRecipe(ROCItems.fractonite_sword, ROCItems.fractonite_pickaxe, ROCItems.fractonite_axe, ROCItems.fractonite_shovel, ROCItems.fractonite_bow, ROCItems.fractonite_arrow, ROCItems.fractonite_stone);
 		
 		GameRegistry.addRecipe(new ItemStack(ROCItems.fish_scale, 2), new Object[]{"F", 'F', Items.fish});
 
