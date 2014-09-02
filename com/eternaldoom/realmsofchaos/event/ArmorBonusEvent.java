cd7ca1221732df739a00def6c8e9133b432bb702
Eternaldoom <mezack@icloud.com>
2014-09-02 09:16:46 -0700

    fixed lava bonus (maybe)

--- com/eternaldoom/realmsofchaos/event/ArmorBonusEvent.java
+++ com/eternaldoom/realmsofchaos/event/ArmorBonusEvent.java
@@ -1,7 +1,7 @@
 package com.eternaldoom.realmsofchaos.event;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
@@ -13,7 +13,6 @@ import cpw.mods.fml.common.gameevent.InputEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
 public class ArmorBonusEvent {
-	public boolean lavaWPressed, lavaAPressed, lavaSPressed, lavaDPressed;
 	public static boolean hasAquatic;
 	@SubscribeEvent
 	public void tickEvent(PlayerTickEvent evt){
@@ -30,25 +29,14 @@ public class ArmorBonusEvent {
 		
 		if (helmet == ROCItems.flame_helmet && chestplate == ROCItems.flame_chestplate && leggings == ROCItems.flame_leggings && boots == ROCItems.flame_boots){
 			evt.player.addPotionEffect(new PotionEffect(12, 20, 0, true));
-			if (evt.player.worldObj.getBlock((int)evt.player.posX, (int)evt.player.posY - 1, (int)evt.player.posZ) == Blocks.lava){
-				if (evt.player.motionX < 0.2f && evt.player.motionX > -0.2f){
-					if(lavaWPressed && evt.player.motionX >= 0){
-						evt.player.motionX += 0.1f;
-						evt.player.motionX *= 1.5f;
-					}
-					
-					if(lavaSPressed && evt.player.motionX <= 0){
-						evt.player.motionX -= 0.1f;
-						evt.player.motionX *= 1.5f;
-					}
-				}
-				
-				if (evt.player.motionZ < 0.2f && evt.player.motionZ > -0.2f){
-					if(lavaAPressed)
-					evt.player.motionZ *= 1.5f;
+			if(evt.player.isInsideOfMaterial(Material.lava)){
+				evt.player.capabilities.isFlying = true;
+			}else{
+				if(!evt.player.capabilities.isCreativeMode){
+					evt.player.capabilities.isFlying = false;
 				}
 			}
-		}
+	}
 		
 		if (helmet == ROCItems.aquatic_helmet && chestplate == ROCItems.aquatic_chestplate && leggings == ROCItems.aquatic_leggings && boots == ROCItems.aquatic_boots){
 			hasAquatic = true;
@@ -67,20 +55,5 @@ public class ArmorBonusEvent {
 	
 	public static boolean getAquatic(){
 		return hasAquatic;
-	}
-
-	@SubscribeEvent
-	public void keyPressed(InputEvent.KeyInputEvent evt){
-		if(Minecraft.getMinecraft().gameSettings.keyBindForward.isPressed()) lavaWPressed = true;
-		else lavaWPressed = false;
-		
-		if(Minecraft.getMinecraft().gameSettings.keyBindLeft.isPressed()) lavaAPressed = true;
-		else lavaAPressed = false;
-		
-		if(Minecraft.getMinecraft().gameSettings.keyBindBack.isPressed()) lavaSPressed = true;
-		else lavaSPressed = false;
-		
-		if(Minecraft.getMinecraft().gameSettings.keyBindRight.isPressed()) lavaDPressed = true;
-		else lavaDPressed = false;
 	}
 }
