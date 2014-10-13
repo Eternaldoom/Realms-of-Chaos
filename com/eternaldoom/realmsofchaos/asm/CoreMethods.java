cf61eb8936a479633169a36f0e0ee9a2f1a316fb
Eternaldoom <mezack@icloud.com>
2014-10-12 17:48:09 -0700

    fixed a bug where metadata items were untextured when held

--- com/eternaldoom/realmsofchaos/asm/CoreMethods.java
+++ com/eternaldoom/realmsofchaos/asm/CoreMethods.java
@@ -39,18 +39,12 @@ public class CoreMethods {
 		}
 	}
 	
-	/**A modified method to include the added dimensions*/
-	public static WorldProvider getProviderForDimension(int dimension)
-    {
-        return (WorldProvider)(dimension == -1 ? new WorldProviderHell() : (dimension == 0 ? new WorldProviderSurface() : (dimension == 1 ? new WorldProviderEnd() : (dimension == 24 ? new WorldProviderIceRuins() : null))));
-    }
-	
 	/**Inserted into RenderItem to allow for customized textures based on use time.*/
 	public static ModelResourceLocation getBowTextures(EntityPlayer player, Item item, ItemStack stack, ModelResourceLocation location){
-        location = new ModelResourceLocation(((ResourceLocation) Item.itemRegistry.getNameForObject(item)).toString(), "inventory");
-
 		if (item instanceof ItemROCBow && player.getItemInUse() != null)
         {
+	        location = new ModelResourceLocation(((ResourceLocation) Item.itemRegistry.getNameForObject(item)).toString(), "inventory");
+
             int i = stack.getMaxItemUseDuration() - player.getItemInUseCount();
             if (i >= 18)
             {
@@ -65,16 +59,6 @@ public class CoreMethods {
                 location = new ModelResourceLocation(Item.itemRegistry.getNameForObject(item) + "_pulling_0", "inventory");
             }
         }
-		
-		if(item == Item.getItemFromBlock(ROCBlocks.oceanstone_bricks)){
-			if(stack.getItemDamage() == 1)location = new ModelResourceLocation("realmsofchaos:cracked_oceanstone_bricks", "inventory");
-			if(stack.getItemDamage() == 2)location = new ModelResourceLocation("realmsofchaos:chiseled_oceanstone_bricks", "inventory");
-		}
-		
-		if(item == Item.getItemFromBlock(ROCBlocks.frozen_stone_bricks)){
-			if(stack.getItemDamage() == 1)location = new ModelResourceLocation("realmsofchaos:cracked_frozen_stone_bricks", "inventory");
-			if(stack.getItemDamage() == 2)location = new ModelResourceLocation("realmsofchaos:chiseled_frozen_stone_bricks", "inventory");
-		}
 		return location;
 	}
 	
