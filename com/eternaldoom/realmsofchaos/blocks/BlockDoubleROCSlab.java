5fce04a2475581e33b7e6380688c6d6de5e4449b
Eternaldoom <mezack@icloud.com>
2014-10-21 14:19:41 -0700

    slab pick block and texture fix

--- com/eternaldoom/realmsofchaos/blocks/BlockDoubleROCSlab.java
+++ com/eternaldoom/realmsofchaos/blocks/BlockDoubleROCSlab.java
@@ -1,7 +1,13 @@
 package com.eternaldoom.realmsofchaos.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockDoubleROCSlab extends BlockROCSlab{
 
@@ -13,5 +19,12 @@ public class BlockDoubleROCSlab extends BlockROCSlab{
 	@Override
 	public boolean isDouble(){
 		return true;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public Item getItem(World world, BlockPos pos){
+		Block b = this == ROCBlocks.netherrack_brick_slab_double ? ROCBlocks.netherrack_brick_slab : (this == ROCBlocks.charwood_slab_double ? ROCBlocks.charwood_slab : (this == ROCBlocks.oceanstone_slab_double ? ROCBlocks.oceanstone_slab : (this == ROCBlocks.ocean_cobblestone_slab_double ? ROCBlocks.ocean_cobblestone_slab : (this == ROCBlocks.oceanstone_brick_slab_double ? ROCBlocks.oceanstone_brick_slab : (this == ROCBlocks.frozen_wood_slab_double ? ROCBlocks.frozen_wood_slab : (this == ROCBlocks.frozen_stone_slab_double ? ROCBlocks.frozen_stone_slab : (this == ROCBlocks.frozen_cobblestone_slab_double ? ROCBlocks.frozen_cobblestone_slab : (this == ROCBlocks.frozen_stone_brick_slab_double ? ROCBlocks.frozen_stone_brick_slab : Blocks.air))))))));
+		return Item.getItemFromBlock(b);
 	}
 }
